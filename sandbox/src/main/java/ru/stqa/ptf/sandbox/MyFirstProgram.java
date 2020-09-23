ce7b73b58415c3821dcca295c75c47de73df2d72
Paulus1957 <Paulus1957@mail.ru>
2020-09-23 23:57:10 +0300

    добавлена функция вычисления площади прямоугольника

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
@@ -4,5 +4,12 @@ public class MyFirstProgram {
 
    public static void main(String[] args) {
       System.out.println("Hello, world");
+      double a = 6;
+      double b = 7;
+       System.out.println("Площадь прямоугольника со сторонами " + a + " и "+ b + " = " + area(a, b));
+
    }
+    public static double area (double a, double b){
+       return a*b;
+    }
 }
