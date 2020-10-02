73b2a1b0a8ecb25b868a073fb3a01f73425d86e6
Paulus1957 <Paulus1957@mail.ru>
2020-10-03 02:08:43 +0300

    ДЗ -7 запуск тестов в Firefox and Chrome

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appManager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appManager/ApplicationManager.java
@@ -4,7 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 import ru.stqa.ptf.addressbook.model.ContactData;
 
 import java.util.concurrent.TimeUnit;
@@ -17,10 +20,23 @@ public class ApplicationManager {
   private   GroupHelper groupHelper ;
   private   SessionHelper sessionHelper ;
   private   ContactHelper contactHelper ;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+
+  }
 
   public void init() {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+      if (browser.equals(BrowserType.FIREFOX)){
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)){
+      wd = new InternetExplorerDriver();
+    }
+    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
     groupHelper = new GroupHelper(wd);
     contactHelper = new ContactHelper(wd);
