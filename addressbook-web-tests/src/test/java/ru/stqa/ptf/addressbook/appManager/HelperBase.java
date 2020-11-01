5d275f9b865c894daf2a13d2464fe4c2a26a8d68
Paulus1957 <Paulus1957@mail.ru>
2020-11-01 14:57:57 +0300

    Revert "Revert "6.1 Добавляем загрузку файла в тест создания групп""
    This reverts commit b2ebc5cdba88645967d752e798e9333b9283b97d.

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appManager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appManager/HelperBase.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
+import java.io.File;
+
 public class HelperBase {
   protected WebDriver wd;
 
@@ -25,6 +27,13 @@ public class HelperBase {
       }
     }
   }
+
+  protected void attach(By locator, File file) {
+    if (file != null) {
+      wd.findElement(locator).sendKeys(file.getAbsolutePath());
+      }
+    }
+
 
   protected boolean isElementPresent(By locator) {
     try {
--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,10 @@ import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.model.ContactData;
 import ru.stqa.ptf.addressbook.model.Contacts;
 
+import javax.sound.midi.Soundbank;
+import java.io.File;
+import java.sql.SQLOutput;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertEquals;
@@ -13,16 +17,19 @@ public class ContactCreationTests extends TestBase {
   @Test(enabled = true)
   public void testContactCreation() throws Exception {
    Contacts before = app.contact().all();
+    File photo = new File("src/test/resources/picture.png");
     ContactData contact = new ContactData().withFirstname("Иван79").withMiddlename("Иванович")
           .withLastname("Иванов").withNickname("Ivan").withAddress("Москва Петровка 38").
                 withMobileCellPhone( "89776666666")
-    .withEmail("IvanMolodec@mail.ru").withGroup("test1");
+    .withEmail("IvanMolodec@mail.ru").withPhoto(photo).withGroup("test1");
     app.contact().createContact(contact);
     assertThat(app.contact().count(), equalTo(before.size()+1));
     Contacts after = app.contact().all();
     assertThat(after, equalTo(
           before.withAdded(contact.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
   }
+
+
   @Test(enabled = true)
   public void testBadContactCreation() throws Exception {
     Contacts before = app.contact().all();
@@ -36,3 +43,17 @@ public class ContactCreationTests extends TestBase {
     assertThat(after, equalTo(before));
   }
 }
+
+
+/* Тест для проверки наличия файла
+
+ @Test
+  public void testCurrentDir(){
+    File currentDir = new File(".");
+    System.out.println(currentDir.getAbsolutePath());
+    File photo = new File("src/test/resources/picture.png");
+    System.out.println(photo.getAbsolutePath());
+    System.out.println(photo.exists());
+  }
+
+ */
