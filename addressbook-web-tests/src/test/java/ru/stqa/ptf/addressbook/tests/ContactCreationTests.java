a98147e5c257c378111976373828c6393311b6fa
Paulus1957 <Paulus1957@mail.ru>
2020-11-01 17:36:18 +0300

    Лекция 6.4. Параметризация тестовых методов(Тест создания групп и контактов)

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/ContactCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.ptf.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.model.ContactData;
 import ru.stqa.ptf.addressbook.model.Contacts;
@@ -7,35 +8,66 @@ import ru.stqa.ptf.addressbook.model.Contacts;
 import javax.sound.midi.Soundbank;
 import java.io.File;
 import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.testng.Assert.assertEquals;
+
 
 public class ContactCreationTests extends TestBase {
 
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[]{new ContactData().withFirstname("Иван1").withMiddlename("Иванович")
+          .withLastname("Иванов").withNickname("Ivan").withAddress("Москва Петровка 38").
+                withMobileCellPhone("89776666666")
+          .withEmail("IvanMolodec@mail.ru").withGroup("test1")});
+    list.add(new Object[]{new ContactData().withFirstname("Иван2").withMiddlename("Иванович")
+          .withLastname("Иванов").withNickname("Ivan").withAddress("Москва Петровка 38").
+                withMobileCellPhone("89776666666")
+          .withEmail("IvanMolodec@mail.ru").withGroup("test1")});
+    list.add(new Object[]{new ContactData().withFirstname("Иван3").withMiddlename("Иванович")
+          .withLastname("Иванов").withNickname("Ivan").withAddress("Москва Петровка 38").
+                withMobileCellPhone("89776666666")
+          .withEmail("IvanMolodec@mail.ru").withGroup("test1")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) throws Exception {
+    Contacts before = app.contact().all();
+    File photo = new File("src/test/resources/picture.png");
+    app.contact().createContact(contact);
+    assertThat(app.contact().count(), equalTo(before.size() + 1));
+    Contacts after = app.contact().all();
+    assertThat(after, equalTo(
+          before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+  }
+
   @Test(enabled = true)
-  public void testContactCreation() throws Exception {
-   Contacts before = app.contact().all();
+  public void testContactCreationWithPhoto() throws Exception {
+    Contacts before = app.contact().all();
     File photo = new File("src/test/resources/picture.png");
     ContactData contact = new ContactData().withFirstname("Иван79").withMiddlename("Иванович")
           .withLastname("Иванов").withNickname("Ivan").withAddress("Москва Петровка 38").
-                withMobileCellPhone( "89776666666")
-    .withEmail("IvanMolodec@mail.ru").withPhoto(photo).withGroup("test1");
+                withMobileCellPhone("89776666666")
+          .withEmail("IvanMolodec@mail.ru").withPhoto(photo).withGroup("test1");
     app.contact().createContact(contact);
-    assertThat(app.contact().count(), equalTo(before.size()+1));
+    assertThat(app.contact().count(), equalTo(before.size() + 1));
     Contacts after = app.contact().all();
     assertThat(after, equalTo(
-          before.withAdded(contact.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
+          before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
-
 
   @Test(enabled = true)
   public void testBadContactCreation() throws Exception {
     Contacts before = app.contact().all();
     ContactData contact = new ContactData().withFirstname("Иван79'").withMiddlename("Иванович'")
           .withLastname("Иванов").withNickname("Ivan").withAddress("Москва Петровка 38").
-                withMobileCellPhone( "89776666666")
+                withMobileCellPhone("89776666666")
           .withEmail("IvanMolodec@mail.ru").withGroup("test1");
     app.contact().createContact(contact);
     assertThat(app.contact().count(), equalTo(before.size()));
@@ -43,6 +75,7 @@ public class ContactCreationTests extends TestBase {
     assertThat(after, equalTo(before));
   }
 }
+
 
 
 /* Тест для проверки наличия файла
--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreationTests.java
@@ -1,9 +1,14 @@
 package ru.stqa.ptf.addressbook.tests;
 
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.model.GroupData;
 import ru.stqa.ptf.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -11,27 +16,34 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() throws Exception {
-
-    app.goTo().GroupPage();
-    Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test5") ;
-    app.group().create(group);
-    assertThat(app.group().count(), equalTo(before.size() + 1));
-    Groups after = app.group().all();
-    assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
+  @DataProvider
+  public Iterator<Object[]> validGroups(){
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
+    list.add(new Object[] {new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
+    list.add(new Object[] {new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+    return list.iterator();
   }
 
-  @Test
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) throws Exception {
+      app.goTo().GroupPage();
+      Groups before = app.group().all();
+      app.group().create(group);
+      assertThat(app.group().count(), equalTo(before.size() + 1));
+      Groups after = app.group().all();
+      assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
+  }
+  @Test(enabled = false)
   public void testBadGroupCreation() throws Exception {
 
     app.goTo().GroupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test5'") ;
+    GroupData group = new GroupData().withName("test5'");
     app.group().create(group);
 
-    assertThat(app.group().count(), equalTo(before.size() ));
+    assertThat(app.group().count(), equalTo(before.size()));
     Groups after = app.group().all();
     assertThat(after, equalTo(before));
   }
