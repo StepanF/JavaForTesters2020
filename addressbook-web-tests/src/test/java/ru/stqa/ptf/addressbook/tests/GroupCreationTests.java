6871defe1454043c43536d7a78c52150a1a13e72
Paulus1957 <Paulus1957@mail.ru>
2020-11-02 00:03:37 +0300

    6.11. Протоколирование (лог-файлы)

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreationTests.java
@@ -23,6 +23,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
+
   @DataProvider
   public Iterator<Object[]> validGroupsFromXml() throws IOException {
     try(BufferedReader reader = new BufferedReader(new FileReader((new File("src/test/resources/groups.xml"))))){
@@ -62,7 +63,6 @@ public class GroupCreationTests extends TestBase {
       assertThat(app.group().count(), equalTo(before.size() + 1));
       Groups after = app.group().all();
       assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
-
   }
   @Test(enabled = false)
   public void testBadGroupCreation() throws Exception {
