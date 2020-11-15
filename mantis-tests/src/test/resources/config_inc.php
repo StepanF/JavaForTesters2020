fa9cfa03bec0769003e83b2aada36a828b2e8f14
Paulus1957 <Paulus1957@mail.ru>
2020-11-15 21:48:20 +0300

    8.6. Получение почты на встроенный в тесты почтовый сервер

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -1,11 +1,16 @@
 <?php
-$g_hostname = 'localhost';
-$g_db_type  = 'mysqli';
-$g_database_name = 'bugtracker';
-$g_db_username   = 'root';
-$g_db_password   = '';
-$g_signup_use_captcha =   ON;
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+$g_default_language = "english";
+$g_signup_use_captcha = OFF;
+$g_default_language = "english";
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
 
 $g_default_timezone       = 'Europe/Berlin';
 
 $g_crypto_master_salt     = 'iifsJlpeYkh9wtoXRLvYyx40ilfrlvHLd6RYoWk7Et4=';
+
